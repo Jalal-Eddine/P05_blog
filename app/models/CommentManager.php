73aeb30be02278514acf18c18f9e3a705066f321
JalalEddine <jalaleddine.elhabbazi@gmail.com>
2021-05-08 20:27:37 +0200

    creating single post page and the comments section: display & creat a comment

---
+++ app/models/CommentManager.php
@@ -0,0 +1,23 @@
+<?php
+require_once("models/Manager.php");
+
+class CommentManager extends Manager
+{
+    public function getComments($postId)
+    {
+        $db = $this->dbConnect();
+        $comments = $db->prepare('SELECT id, title, content, DATE_FORMAT(created_date, \'%d/%m/%Y à %Hh%imin%ss\') AS comment_date FROM comment WHERE post_id = ? ORDER BY created_date DESC');
+        $comments->execute(array($postId));
+
+        return $comments;
+    }
+
+    public function postComment($postId, $title, $content)
+    {
+        $db = $this->dbConnect();
+        $comments = $db->prepare('INSERT INTO comment(post_id, title, content, user_id,comment_status_id, created_date) VALUES(?, ?, ?,1,1, NOW())');
+        $affectedLines = $comments->execute(array($postId, $title, $content));
+
+        return $affectedLines;
+    }
+}
--- app/models/Manager.php
+++ app/models/Manager.php
@@ -11,7 +11,7 @@ class Manager
           $db = new PDO("mysql:host=$servername;dbname=P05_blog", $username, $password);
           // set the PDO error mode to exception
           $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-          echo "Connected successfully";
+          // echo "Connected successfully";
           return $db;
         } 
     }
